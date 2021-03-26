5ec044dbdd7b78c4e1eb37042d2a132001236db2
Blanark <git@inrevo.co.uk>
2021-03-26 22:42:54 +0000

    modified:   config/lorekeeper/extension_tracker.php

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -46,5 +46,61 @@ return [
         ]),
         'version' => '1.1.0',
     ],
-
+    'extension_tracker' => [
+	'key' => 'character_links',
+	'wiki_key' => 'Character_Links',
+	'creators' => json_encode([
+	    'junijwi' => 'https://github.com/junijwi/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'character_lineages',
+	'wiki_key' => 'Character_Lineages',
+	'creators' => json_encode([
+	    'DeeP-ci' => 'https://github.com/DeeP-ci/',
+            'Pure09' => 'https://github.com/Pure09/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'trade_listings',
+	'wiki_key' => 'Trade_Listings',
+	'creators' => json_encode([
+	    'Mercury' => 'https://github.com/Mercury/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'world_expansion',
+	'wiki_key' => 'World_Expansion',
+	'creators' => json_encode([
+	    'Uri' => 'https://github.com/Preimpression/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'organized_trait_dropdown',
+	'wiki_key' => 'Organized_Trait_Dropdown',
+	'creators' => json_encode([
+	    'Draginraptor' => 'https://github.com/Draginraptor/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'page_categories',
+	'wiki_key' => 'Page_Categories',
+	'creators' => json_encode([
+	    'Junijwi' => 'https://github.com/junijwi/',
+	]),
+	'version' => '1.0.0',
+    ],
+    'extension_tracker' => [
+	'key' => 'oldest_sorting',
+	'wiki_key' => 'Oldest_Sorting',
+	'creators' => json_encode([
+	    'Draginraptor' => 'https://github.com/Draginraptor/',
+	]),
+	'version' => '1.0.0',
+    ],
 ];
